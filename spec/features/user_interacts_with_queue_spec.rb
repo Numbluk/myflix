42701a42607027f49b68ff62636cf25c7836db83
Durendal <lukas.nimmo@gmail.com>
2016-05-27 00:19:20 -0500

    Added feature spec for user interaction with my queue

--- spec/features/user_interacts_with_queue_spec.rb
+++ spec/features/user_interacts_with_queue_spec.rb
@@ -1,42 +1,48 @@
-# require 'spec_helper'
-#
-# feature 'User interacts with queue' do
-#   scenario 'user adds and reorders videos in queue' do
-#     drama = Fabricate(:category)
-#     south_park = Fabricate(:video, id: 1, title: 'South Park', category: drama)
-#     warcraft = Fabricate(:video, id: 2, title: 'Warcraft', category: drama)
-#     fargo = Fabricate(:video, id: 3, title: 'Fargo', category: drama)
-#
-#     sign_in
-#
-#     find("a[href='/videos/#{warcraft.id}']").click
-#     expect(page).to have_content(warcraft.title)
-#     click_link('+ My Queue')
-#     expect(page).to have_content(warcraft.title)
-#
-#     find("a[href='/videos/#{warcraft.id}']").click
-#     expect(page).not_to have_content('+ My Queue')
-#
-#     visit('/home')
-#     find("a[href='/videos/#{south_park.id}']").click
-#     click_link('+ My Queue')
-#
-#     visit('/home')
-#     find("a[href='/videos/#{fargo.id}']").click
-#     click_link('+ My Queue')
-#     binding.pry
-#     within(:xpath, "//tr[contains(.,'#{warcraft.title}')]") do
-#       fill_in "queue_ids_with_positions[#{warcraft.id}][]", with: 3
-#     end
-#     within(:xpath, "//tr[contains(.,'#{south_park.title}')]") do
-#       fill_in 'queue_ids_with_positions[][position]', with: 2
-#     end
-#     within(:xpath, "//tr[contains(.,'#{fargo.title}')]") do
-#       fill_in 'queue_ids_with_positions[][position]', with: 1
-#     end
-#
-#     click_button 'Update Instant Queue'
-#
-#     # :xpath, "//tr[contains(.,'#{south_park.title}')]//input[@type='text']"
-#   end
-# end
+require 'spec_helper'
+
+feature 'User interacts with queue' do
+  scenario 'user adds and reorders videos in queue' do
+    drama = Fabricate(:category)
+    south_park = Fabricate(:video, id: 1, title: 'South Park', category: drama)
+    warcraft = Fabricate(:video, id: 2, title: 'Warcraft', category: drama)
+    fargo = Fabricate(:video, id: 3, title: 'Fargo', category: drama)
+
+    sign_in
+
+    add_video_to_queue(warcraft)
+    expect(page).to have_content(warcraft.title)
+
+    visit video_path(warcraft)
+    expect(page).not_to have_content('+ My Queue')
+
+    add_video_to_queue(south_park)
+    add_video_to_queue(fargo)
+
+    fill_in_item_position_in_queue(warcraft, 3)
+    fill_in_item_position_in_queue(south_park, 2)
+    fill_in_item_position_in_queue(fargo, 1)
+
+    click_button 'Update Instant Queue'
+
+    expect_video_at_position(south_park, '2')
+    expect_video_at_position(warcraft, '3')
+    expect_video_at_position(fargo, '1')
+  end
+
+  def add_video_to_queue(video)
+    visit home_path
+    find("a[href='/videos/#{video.id}']").click
+    click_link('+ My Queue')
+  end
+
+  def fill_in_item_position_in_queue(video, position)
+    within(:xpath, "//tr[contains(.,'#{video.title}')]") do
+      fill_in 'queue_items[][position]', with: position
+    end
+  end
+
+  def expect_video_at_position(video, position)
+    expect(find(:xpath, "//tr[contains(., '#{video.title}')]//input[@type='text']").value).to eq(position)
+  end
+
+end
